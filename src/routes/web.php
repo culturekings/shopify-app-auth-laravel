a579751ae9da9fa4b41edabd0807085c067a2765
Jonathan Bird <jonobird1@gmail.com>
2017-05-09 11:36:00 +1000

    Test middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,8 @@
 Route::group(['namespace' => 'CultureKings\ShopifyAuth\Http\Controllers'], function () {
     Route::get('shopify-auth/{appName}/install', 'AuthController@installShop');
     Route::get('shopify-auth/{appName}/auth/callback', 'AuthController@processOAuthResultRedirect');
+});
+
+Route::group(['namespace' => 'CultureKings\ShopifyAuth\Http\Controllers', 'middleware' => 'web'], function () {
     Route::get('shopify-auth/{appName}/install/success', 'AuthController@getSuccessPage');
 });
